e0c913354b46791c62a5f74eae54ac59dd192c75
Malcom Li <mx4li@uwaterloo.ca>
2016-06-11 13:20:23 -0400

    set up basic server

---
+++ index.js
@@ -0,0 +1,21 @@
+var app = require("express")();
+var bodyParser = require('body-parser');
+var port = process.env.PORT || 8080;
+var request = ('request');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+app.get('/censor', function(req, res) {
+    res.json({
+        success: true
+    });
+});
+
+var server = app.listen(port, function(cb) {
+    var port = server.address().port;
+    console.log('Listening on port http://localhost:%s', port);
+});
+
