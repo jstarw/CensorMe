4f59e95ebc8e5e9e479c17b22804a99631c47274
Malcom Li <mx4li@uwaterloo.ca>
2016-06-11 13:59:25 -0400

    added link to public file

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var bodyParser = require('body-parser');
 var port = process.env.PORT || 8080;
 var request = ('request');
 
+app.use('/', express.static(__dirname+ '/public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
   extended: true
